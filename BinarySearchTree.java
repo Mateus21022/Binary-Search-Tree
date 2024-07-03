cc35de1aa8560a13e784aea7f8067432148ea6ed
Mateus Rocha <135074699+Mateus21022@users.noreply.github.com>
2024-07-03 17:12:24 -0300

    Add files via upload

---
+++ BinarySearchTree.java
@@ -0,0 +1,93 @@
+package pp;
+
+public class BinarySearchTree {
+	  private BinaryNode root;
+	  private class BinaryNode {
+	     private int element;
+	     private BinaryNode left;
+	     private BinaryNode right;
+	     BinaryNode(int element,BinaryNode left,BinaryNode right) {
+	          this.element=element;
+	          this.left=left;
+	          this.right=right;
+	      }
+	  }
+	  public BinarySearchTree( ) {
+	     root=null;
+	  }
+	  public void clear( ) {
+	     root=null;
+	  }
+
+	  public boolean isEmpty( ) {
+	     return (root==null);
+	  }
+public void insert(int x) {
+	root=insert(x,root);
+}
+private BinaryNode insert( int x, BinaryNode t ) {
+      if ( t == null )
+         t = new BinaryNode( x, null, null );
+      else if( x < t.element )
+         t.left = insert( x, t.left );
+      else if( x > t.element ) 
+         t.right = insert( x, t.right ); 
+      return t;
+}
+
+				//ORDEM
+    
+	  /*public void print( ) {
+		     if( isEmpty( ))
+		        System.out.println( "Arvore Vazia" );
+		     else
+		        print( root );
+	  } 
+
+	  private void print( BinaryNode t ) {
+		  
+		  if ( t != null ) {
+			  print( t.left );
+		      System.out.println( t.element );
+		      print( t.right );
+		  }
+	  }*/
+
+					//PRÉ-ORDEM
+
+		public void print() {
+			if(isEmpty()) {
+				System.out.println("Árvore Vazia");
+			} else {
+				print(root);
+			}
+		}
+		
+		private void print( BinaryNode t ) {
+		  
+		  if ( t != null ) {
+      System.out.println( t.element );
+       print( t.left );
+		      print( t.right );
+		  }
+	  }
+
+				/*//PÓS-ORDEM
+
+		public void print() {
+			if(isEmpty()) {
+				System.out.println("Árvore Vazia");
+			} else {
+				print(root);
+			}
+		}
+		
+		private void print(BinaryNode t) {
+			if( t != null) {
+				print(t.left);
+				print(t.right);
+				System.out.println(t.element); 
+					
+			}
+		}*/
+}
---
+++ main.java
@@ -0,0 +1,27 @@
+package binarytree;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		BinarySearchTree myapp = new BinarySearchTree();
+		
+		myapp.insert(8);
+		myapp.insert(4);
+		myapp.insert(3);
+		myapp.insert(6);
+		myapp.insert(5);
+		myapp.insert(7);
+		myapp.insert(15);
+		myapp.insert(12);
+		myapp.insert(16);
+		myapp.insert(10);
+		myapp.insert(14);
+		myapp.insert(9);
+		myapp.insert(11);
+		
+		myapp.print();
+		
+	}
+
+}
